3e739c3a4e235db77d466fe280fff88bd767db83
ylee332 <ylee@us.ibm.com>
2015-11-20 10:55:16 -0600

    Angela's Code

--- src/main/java/helloworld/HelloWorldSpeechlet.java
+++ src/main/java/helloworld/HelloWorldSpeechlet.java
@@ -7,12 +7,17 @@
 
     or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
+
 package helloworld;
 
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.amazon.speech.slu.Intent;
+import com.amazon.speech.slu.Slot;
 import com.amazon.speech.speechlet.IntentRequest;
 import com.amazon.speech.speechlet.LaunchRequest;
 import com.amazon.speech.speechlet.Session;
@@ -26,10 +31,14 @@ import com.amazon.speech.ui.Reprompt;
 import com.amazon.speech.ui.SimpleCard;
 
 /**
- * This sample shows how to create a simple speechlet for handling speechlet requests.
+ * This sample shows how to create a simple speechlet for handling intent requests and managing
+ * session interactions.
  */
 public class HelloWorldSpeechlet implements Speechlet {
     private static final Logger log = LoggerFactory.getLogger(HelloWorldSpeechlet.class);
+
+    private static final String ACCOUNT_KEY = "ACCOUNT";
+    private static final String ACCOUNT_SLOT = "Account";
 
     @Override
     public void onSessionStarted(final SessionStartedRequest request, final Session session)
@@ -53,12 +62,17 @@ public class HelloWorldSpeechlet implements Speechlet {
         log.info("onIntent requestId={}, sessionId={}", request.getRequestId(),
                 session.getSessionId());
 
+        // Get intent from the request object.
         Intent intent = request.getIntent();
         String intentName = (intent != null) ? intent.getName() : null;
 
-        if ("HelloWorldIntent".equals(intentName)) {
-            return getHelloResponse();
-        } else if ("HelpIntent".equals(intentName)) {
+        // Note: If the session is started with an intent, no welcome message will be rendered;
+        // rather, the intent specific response will be returned.
+        if  ("MyBalanceIntent".equals(intentName)) {
+            return getBalanceResponse(intent, session);
+        }else if ("MyAccountIntent".equals(intentName)) {
+            return setAccountResponse(intent, session);
+        }else if ("HelpIntent".equals(intentName)) {
             return getHelpResponse();
         } else {
             throw new SpeechletException("Invalid Intent");
@@ -76,68 +90,141 @@ public class HelloWorldSpeechlet implements Speechlet {
     /**
      * Creates and returns a {@code SpeechletResponse} with a welcome message.
      *
-     * @return SpeechletResponse spoken and visual response for the given intent
+     * @return SpeechletResponse spoken and visual welcome message
      */
     private SpeechletResponse getWelcomeResponse() {
-        String speechText = "Welcome to the Alexa Skills Kit, you can say hello";
+        // Create the welcome message.
+        String speechText =
+                "Welcome to the Alexa Skills Kit sample. To check your balance, please tell me the account you are interested in by "
+                        + "saying, what's my savings account balance";
+        String repromptText =
+                "Please tell me the account you are interested by saying, what's my savings account balance";
 
-        // Create the Simple card content.
-        SimpleCard card = new SimpleCard();
-        card.setTitle("HelloWorld");
-        card.setContent(speechText);
-
-        // Create the plain text output.
-        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-        speech.setText(speechText);
-
-        // Create reprompt
-        Reprompt reprompt = new Reprompt();
-        reprompt.setOutputSpeech(speech);
-
-        return SpeechletResponse.newAskResponse(speech, reprompt, card);
+        return getSpeechletResponse(speechText, repromptText, true);
     }
-
     /**
-     * Creates a {@code SpeechletResponse} for the hello intent.
+     * Creates a {@code SpeechletResponse} for the intent and stores the extracted color in the
+     * Session.
      *
-     * @return SpeechletResponse spoken and visual response for the given intent
+     * @param intent
+     *            intent for the request
+     * @return SpeechletResponse spoken and visual response the given intent
      */
-    private SpeechletResponse getHelloResponse() {
-        String speechText = "Hello world";
+    private SpeechletResponse setAccountResponse(final Intent intent, final Session session) {
+        // Get the slots from the intent.
+        Map<String, Slot> slots = intent.getSlots();
 
-        // Create the Simple card content.
-        SimpleCard card = new SimpleCard();
-        card.setTitle("HelloWorld");
-        card.setContent(speechText);
+        // Get the account slot from the list of slots.
+        Slot listOfAccountsSlot = slots.get(ACCOUNT_SLOT);
+        String speechText, repromptText;
 
-        // Create the plain text output.
-        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-        speech.setText(speechText);
+        // Check for account and create output to user.
+        if (listOfAccountsSlot != null) {
+            // Store the user's account in the Session and create response.
+            String theAccount = listOfAccountsSlot.getValue();
+            session.setAttribute(ACCOUNT_KEY, theAccount);
+            speechText =
+                    String.format("I now know you are asking for your %s account. You can ask me your "
+                            + "balance, what's my %s account balalnce", theAccount, theAccount);
+            repromptText =
+                    String.format("You can ask me your balace by saying, what's my %s account balance?", theAccount);
 
-        return SpeechletResponse.newTellResponse(speech, card);
+        } else {
+            // Render an error since we don't know what the users desired account is.
+            speechText = "I'm not sure which account you are talking about, please try again";
+            repromptText =
+                    "I'm not sure which account you are talking about. You can tell me the account you want information about by "
+                            + "saying, what's my savings account balance?";
+        }
+        return getSpeechletResponse(speechText, repromptText, true);
     }
 
     /**
-     * Creates a {@code SpeechletResponse} for the help intent.
+     * Creates a {@code SpeechletResponse} for the intent and get the user's favorite color from the
+     * Session.
      *
-     * @return SpeechletResponse spoken and visual response for the given intent
+     * @param intent
+     *            intent for the request
+     * @return SpeechletResponse spoken and visual response for the intent
+     */
+    private SpeechletResponse getBalanceResponse(final Intent intent, final Session session) {
+        String speechText;
+        boolean isAskResponse = false;
+
+        // Get the user's requested account from the session.
+        String theAccount = (String) session.getAttribute(ACCOUNT_KEY);
+
+        // Check to make sure user's requested account is in the session
+        if (StringUtils.isNotEmpty(theAccount)) {
+            speechText = String.format("Your %s account balance is $123 Goodbye.", theAccount);
+        } else {
+            // Since the user's account is not set render an error message.
+            speechText =
+                    "I'm not sure what account you are talking about. You can say, give me the balance for my savings, checking or credit card account.";
+            isAskResponse = true;
+        }
+
+        return getSpeechletResponse(speechText, speechText, isAskResponse);
+    }
+
+
+    /**
+     * Returns a response for the help intent.
      */
     private SpeechletResponse getHelpResponse() {
-        String speechText = "You can say hello to me!";
+        String speechText =
+                "You can ask me for one of your account balances by saying "
+                        + "what is my savings, checking or credit card account balance?";
+        String repromptText =
+                    "I'm not sure which account you are talking about. You can tell me the account you want information about by "
+                            + "saying, what's my savings account balance?";
 
+          return getSpeechletResponse(speechText, repromptText, true);
+    }
+    /**
+     * Returns a Speechlet response for a speech and reprompt text.
+     */
+    private SpeechletResponse getSpeechletResponse(String speechText, String repromptText,
+            boolean isAskResponse) {
         // Create the Simple card content.
         SimpleCard card = new SimpleCard();
-        card.setTitle("HelloWorld");
+        card.setTitle("Snapshot");
         card.setContent(speechText);
 
         // Create the plain text output.
         PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
         speech.setText(speechText);
 
-        // Create reprompt
-        Reprompt reprompt = new Reprompt();
-        reprompt.setOutputSpeech(speech);
+        if (isAskResponse) {
+            // Create reprompt
+            PlainTextOutputSpeech repromptSpeech = new PlainTextOutputSpeech();
+            repromptSpeech.setText(repromptText);
+            Reprompt reprompt = new Reprompt();
+            reprompt.setOutputSpeech(repromptSpeech);
 
-        return SpeechletResponse.newAskResponse(speech, reprompt, card);
+            return SpeechletResponse.newAskResponse(speech, reprompt, card);
+
+        } else {
+            return SpeechletResponse.newTellResponse(speech, card);
+        }
+    }
+
+        /**
+     * Wrapper for creating the Ask response from the input strings.
+     *
+     * @param stringOutput
+     *            the output to be spoken
+     * @param repromptText
+     *            the reprompt for if the user doesn't reply or is misunderstood.
+     * @return SpeechletResponse the speechlet response
+     */
+    private SpeechletResponse newAskResponse(String stringOutput, String repromptText) {
+        PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+        outputSpeech.setText(stringOutput);
+        PlainTextOutputSpeech repromptOutputSpeech = new PlainTextOutputSpeech();
+        repromptOutputSpeech.setText(repromptText);
+        Reprompt reprompt = new Reprompt();
+        reprompt.setOutputSpeech(repromptOutputSpeech);
+        return SpeechletResponse.newAskResponse(outputSpeech, reprompt);
     }
 }
